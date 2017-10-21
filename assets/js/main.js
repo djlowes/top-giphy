672d74068c62965160aefc19b894a025542d25b7
David <djlowes@gmail.com>
2017-10-21 16:30:01 -0700

    starter code

---
+++ assets/js/main.js
@@ -0,0 +1,3 @@
+var topics = ["Bruce Willis", "Nicole Kidman", "John Travolta", "Denzel Washington", "Julia Roberts", "Tom Hanks", "Samuel L Jackson", "Scarlett Johansson", "Angelina Jolie", "George Clooney", "Brad Pitt", "Sandra Bullock"]
+var authKey = "k2trtEooeVbLW61C7gpJYzaFTXJhfvzh";
+var queryURLBase = "" + authKey;
